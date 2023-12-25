8685e3380c881915906cdc29c1a2bcd095d36882
Dean Abner Julian <dabnerjulian@yahoo.com>
2023-12-26 03:51:33 +0700

    Starting point

---
+++ app/Http/Controllers/Api/DailyItemsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\DailyItemResources;
+use App\Models\DailyItem;
+use Illuminate\Http\Request;
+
+class DailyItemsController extends Controller
+{
+    public function index() {
+        $items = DailyItemResources::collection(DailyItem::paginate());
+
+        return $items;
+    }
+}
